8ab6e9a6cc1862e04dbb40158a102c9986bb4b4c
Ashton Dunderdale <ashtondunderdale@gmail.com>
2024-05-23 15:59:20 +0100

    added multiplayer

--- SocketGame/Program.cs
+++ SocketGame/Program.cs
@@ -1,115 +1,186 @@
-﻿using System.Net.Sockets;
+﻿using System;
+using System.Net.Sockets;
 using System.Net;
+using System.Text;
+using System.Threading;
 
-namespace socketGame;
-
-internal class Program
+namespace socketGame
 {
-    static void Main()
+    internal class Program
     {
-        Console.CursorVisible = false;
-
-        Console.Write("Host(1) or Connect(2): ");
-        var option = Console.ReadLine();
-
-        if (option == "1")
-            HostServer();
-
-        else if (option == "2")
-            ConnectToServer();
-
-
-        var player = new Player(0, 0);
-
-        var keyListenerThread = new Thread(() => KeyListener(player));
-        keyListenerThread.Start();
-
-
-        while (true)
+        static void Main()
         {
+            Console.CursorVisible = false;
 
+            Console.Write("Host(1) or Connect(2): ");
+            var option = Console.ReadLine();
 
+            if (option == "1")
+                HostServer();
+            else if (option == "2")
+                ConnectToServer();
+        }
+
+        static void HostServer()
+        {
+            var ipString = "192.168.129.48";
+            var port = 8000;
+
+            Console.WriteLine($"\nHosting Server: {ipString} on port {port}");
+            var server = new TcpListener(IPAddress.Parse(ipString), port);
+            server.Start();
+
+            Console.WriteLine("Waiting for client connection.");
+            TcpClient client = server.AcceptTcpClient();
+            Console.WriteLine($"\nClient Connected: {client.Client.RemoteEndPoint}");
+
+            var player = new Player(0, 0);
+            var opponent = new Player(10, 10);
+
+            var keyListenerThread = new Thread(() => KeyListener(player, client));
+            keyListenerThread.Start();
+
+            var clientThread = new Thread(() => HandleClient(client, opponent));
+            clientThread.Start();
+        }
+
+        static void ConnectToServer()
+        {
+            Console.Write("\nEnter IP: ");
+            var ip = Console.ReadLine();
+
+            Console.Write("Enter Port: ");
+            var port = Console.ReadLine();
+
+            var client = new TcpClient();
+            client.Connect(IPAddress.Parse(ip), int.Parse(port));
+            Console.Write("Connected.");
+
+            var player = new Player(10, 10);
+            var opponent = new Player(0, 0);
+
+            var keyListenerThread = new Thread(() => KeyListener(player, client));
+            keyListenerThread.Start();
+
+            var serverThread = new Thread(() => HandleServer(client, opponent));
+            serverThread.Start();
+        }
+
+        static void HandleClient(TcpClient client, Player opponent)
+        {
+            var stream = client.GetStream();
+            var buffer = new byte[256];
+
+            while (true)
+            {
+                var bytesRead = stream.Read(buffer, 0, buffer.Length);
+
+                if (bytesRead > 0)
+                {
+                    var message = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+                    var position = message.Split(',');
+
+                    opponent.X = int.Parse(position[0]);
+                    opponent.Y = int.Parse(position[1]);
+                    opponent.previousX = int.Parse(position[2]);
+                    opponent.previousY = int.Parse(position[3]);
+
+                    DrawPlayer(opponent);
+                }
+            }
+        }
+
+        static void HandleServer(TcpClient client, Player opponent)
+        {
+            var stream = client.GetStream();
+            var buffer = new byte[256];
+
+            while (true)
+            {
+                var bytesRead = stream.Read(buffer, 0, buffer.Length);
+
+                if (bytesRead > 0)
+                {
+                    var message = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+                    var position = message.Split(',');
+
+                    opponent.X = int.Parse(position[0]);
+                    opponent.Y = int.Parse(position[1]);
+                    opponent.previousX = int.Parse(position[2]);
+                    opponent.previousY = int.Parse(position[3]);
+
+                    DrawPlayer(opponent);
+                }
+            }
+        }
+
+        static void KeyListener(Player player, TcpClient client)
+        {
+            var stream = client.GetStream();
+
+            DrawPlayer(player);
+
+            while (true)
+            {
+                player.Move();
+
+                var message = $"{player.X},{player.Y},{player.previousX},{player.previousY}";
+                var buffer = Encoding.ASCII.GetBytes(message);
+                stream.Write(buffer, 0, buffer.Length);
+
+                DrawPlayer(player);
+            }
+        }
+
+        static void DrawPlayer(Player player)
+        {
+            Console.SetCursorPosition(player.previousX, player.previousY);
+            Console.Write(' ');
+
+            Console.SetCursorPosition(player.X, player.Y);
+            Console.Write(player.Symbol);
         }
     }
 
-    static void HostServer()
+    internal class Player
     {
-        Console.WriteLine("\nHosting Server: 127.0.0.0 on port 80");
-        var server = new TcpListener(IPAddress.Parse("127.0.0.1"), 80);
-        server.Start();
+        public int X { get; set; }
+        public int Y { get; set; }
 
-        Console.WriteLine("Waiting for client connection.");
-        TcpClient client = server.AcceptTcpClient();
-        Console.WriteLine($"Client Connected: {client.Client.RemoteEndPoint}");
-    }
+        public int previousX { get; set; }
+        public int previousY { get; set; }
 
-    static void ConnectToServer()
-    {
-        Console.Write("\nEnter IP: ");
-        var ip = Console.ReadLine();
+        public char Symbol = '@';
+        public ConsoleColor Color = ConsoleColor.DarkMagenta;
 
-        Console.Write("Enter Port: ");
-        var port = Console.ReadLine();
-
-        var client = new TcpClient();
-        client.Connect(IPAddress.Parse(ip), int.Parse(port));
-    }
-
-    static void KeyListener(Player player)
-    {
-        Console.SetCursorPosition(player.X, player.Y);
-        Console.Write(player.Symbol);
-
-        while (true)
+        public Player(int x, int y)
         {
-            player.Move();
-        }
-    }
-}
-internal class Player
-{
-    public int X { get; set; }
-
-    public int Y { get; set; }
-
-    public char Symbol = '@';
-
-    public ConsoleColor Color = ConsoleColor.DarkMagenta;
-
-    public Player(int x, int y)
-    {
-        X = x;
-        Y = y;
-    }
-
-    public void Move()
-    {
-        var originalX = X;
-        var originalY = Y;
-
-        var key = Console.ReadKey(intercept: true);
-
-        switch (key.Key)
-        {
-            case ConsoleKey.UpArrow:
-                if (Y > 0) Y--;
-                break;
-            case ConsoleKey.DownArrow:
-                if (Y < Console.WindowHeight - 1) Y++;
-                break;
-            case ConsoleKey.LeftArrow:
-                if (X > 0) X--;
-                break;
-            case ConsoleKey.RightArrow:
-                if (X < Console.WindowWidth - 1) X++;
-                break;
+            X = x;
+            Y = y;
         }
 
-        Console.SetCursorPosition(originalX, originalY);
-        Console.Write(' ');
+        public void Move()
+        {
+            previousX = X;
+            previousY = Y;
 
-        Console.SetCursorPosition(X, Y);
-        Console.Write(Symbol);
+            var key = Console.ReadKey(intercept: true);
+
+            switch (key.Key)
+            {
+                case ConsoleKey.UpArrow:
+                    if (Y > 0) Y--;
+                    break;
+                case ConsoleKey.DownArrow:
+                    if (Y < Console.WindowHeight - 1) Y++;
+                    break;
+                case ConsoleKey.LeftArrow:
+                    if (X > 0) X--;
+                    break;
+                case ConsoleKey.RightArrow:
+                    if (X < Console.WindowWidth - 1) X++;
+                    break;
+            }
+        }
     }
-
 }
